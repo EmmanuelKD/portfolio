20e263b43466130a67aea6e4ebce1e6fa3a444fd
EatSalone <eatsalone2022@gmail.com>
2022-10-16 02:37:24 +0000

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import svgr from '@honkhonk/vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ svgr(),react()]
+  base: "/portfolio/",
+  plugins: [svgr(), react()]
 })
